cecad87618dd0500d0d214226efc18b7710fdb52
Mateus Castellar <mateuscastellar@outlook.com>
2022-12-03 00:40:46 -0300

    feat: Pagina inicial do usuario

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import './HomePage.css'
+
+const HomePage = () => {
+  return (
+    <div className="home">
+        <h1>Secret Word</h1>
+        <p>Clique no botão abaixo para começar a jogar</p>
+        <button>Começar o jogo</button>
+    </div>
+  )
+}
+
+export default HomePage
